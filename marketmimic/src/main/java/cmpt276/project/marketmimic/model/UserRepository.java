518155d3b79b541b8cfbb8af7289d215172ab79e
psn4 <psn4@sfu.ca>
2024-06-28 16:40:04 -0700

    removed useless import statement in UserRepository

--- marketmimic/src/main/java/cmpt276/project/marketmimic/model/UserRepository.java
+++ marketmimic/src/main/java/cmpt276/project/marketmimic/model/UserRepository.java
@@ -1,6 +1,5 @@
 package cmpt276.project.marketmimic.model;
 
-import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
