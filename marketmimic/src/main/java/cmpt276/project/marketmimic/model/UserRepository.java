73aaa0142aa9675f344e9275f3dec855c9fc0b39
psn4 <psn4@sfu.ca>
2024-06-28 12:16:47 -0700

    should be runnable now

--- marketmimic/src/main/java/cmpt276/project/marketmimic/model/UserRepository.java
+++ marketmimic/src/main/java/cmpt276/project/marketmimic/model/UserRepository.java
@@ -1,7 +1,7 @@
-package cmpt276.project.marketmimic.model;
+// package cmpt276.project.marketmimic.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+// public interface UserRepository extends JpaRepository<User, Long>{
     
-}
+// }
