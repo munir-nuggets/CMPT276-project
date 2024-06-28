6d3162a7d41467c6a5d9962152b11c1461ed1fb5
munir <munir.adam10@gmail.com>
2024-06-28 12:57:54 -0700

    basic login

--- marketmimic/src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
+++ marketmimic/src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
@@ -1,7 +1,46 @@
 package cmpt276.project.marketmimic.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cmpt276.project.marketmimic.model.User;
+import cmpt276.project.marketmimic.model.UserRepository;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod; 
+import java.util.Map;
+
+
 
 @Controller
 public class loginController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @PostMapping("/usersignup")
+    public String userSignup(@RequestParam Map<String, String> entity) {
+        String username = entity.get("username");
+        String password = entity.get("password");
+        String email = entity.get("email");
+        User user = new User(username, password, email);
+        userRepo.save(user);
+        return "homepage";
+    }
+    
+
+    @GetMapping("/userlogin")
+    public String userLogin(@RequestBody String entity) {
+        
+        return "homepage";
+    }
+    @RequestMapping("home")
+    public String homepage() {
+        return "/homepage";
+    }
+    
     
 }
