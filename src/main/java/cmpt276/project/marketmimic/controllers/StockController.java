9ea8a6e5ac0a888d5bd27bebcc1e28ed426002ef
psn4 <psn4@sfu.ca>
2024-07-12 12:32:37 -0700

    added ability to create line charts from historical data

---
+++ src/main/java/cmpt276/project/marketmimic/controllers/StockController.java
@@ -0,0 +1,35 @@
+package cmpt276.project.marketmimic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cmpt276.project.marketmimic.model.ChartService;
+import cmpt276.project.marketmimic.model.PolygonService;
+import cmpt276.project.marketmimic.model.StockDataResponse;
+
+import java.io.IOException;
+
+@Controller
+public class StockController {
+
+    private final PolygonService polygonService;
+    private final ChartService chartService;
+
+    public StockController(PolygonService polygonService, ChartService chartService) {
+        this.polygonService = polygonService;
+        this.chartService = chartService;
+    }
+
+    @GetMapping("/stock/chart")
+    public String getStockChart(@RequestParam String ticker) {
+        StockDataResponse stockDataResponse = polygonService.getStockData(ticker);
+        try {
+            chartService.createStockChart(stockDataResponse, ticker);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error creating chart";
+        }
+        return "Chart created successfully";
+    }
+}
