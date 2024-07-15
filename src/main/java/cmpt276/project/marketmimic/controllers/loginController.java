212760f737f3e2adf5c588d68a82318db1935896
musumadaan <muskanmadaan2012@gmail.com>
2024-07-14 22:59:46 -0700

    Add reset password file

--- src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
+++ src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
@@ -1,13 +1,19 @@
 package cmpt276.project.marketmimic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.ui.Model;
 import cmpt276.project.marketmimic.model.User;
+import cmpt276.project.marketmimic.model.PasswordResetToken;
 import cmpt276.project.marketmimic.model.Role;
 import cmpt276.project.marketmimic.model.RoleRepository;
 import cmpt276.project.marketmimic.model.UserRepository;
+import cmpt276.project.marketmimic.service.EmailService;
+import cmpt276.project.marketmimic.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
@@ -22,6 +28,10 @@ public class loginController {
     private UserRepository userRepo;
     @Autowired
     private RoleRepository roleRepo;
+    @Autowired
+    private EmailService emailService;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/usersignup")
     public String userSignup(@RequestParam Map<String, String> entity) {
@@ -97,5 +107,51 @@ public class loginController {
     public String homepage() {
         createAdminIfDoesntExist();
         return "homepage";
+    }
+
+    @GetMapping("/forgot-password")
+    public String showForgotPasswordForm(){
+        return "forgot-password";
+    }
+
+    @PostMapping("/forgot-password")
+    public String processForgotPassword(HttpServletRequest request, @RequestParam("email") String email, Model model) {
+        Optional<User> optionalUser = userRepo.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            PasswordResetToken token = tokenService.createToken(user);
+
+            String resetLink = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/reset-password?token=" + token.getToken();
+            emailService.sendEmail(user.getEmail(), "Password Reset Request", "Click the link to reset your password: " + resetLink);
+
+            model.addAttribute("message", "We have sent a reset password link to your email.");
+        } else {
+            model.addAttribute("error", "No account found with the provided email.");
+        }
+        return "forgot-password";
+    }
+
+    @GetMapping("/reset-password")
+    public String showResetPasswordPage(@RequestParam("token") String token, Model model) {
+        // Pass the token to the model so it can be included in the form
+        model.addAttribute("token", token);
+        return "reset-password";
+    }
+    @PostMapping("/reset-password")
+    public String resetPassword(@RequestParam("token") String token, @RequestParam("newPassword") String newPassword) {
+            Optional<PasswordResetToken> resetTokenOptional = tokenService.validatePasswordResetToken(token);
+    
+            if (!resetTokenOptional.isPresent()) {
+                return "forgot-password";
+            }
+    
+            PasswordResetToken resetToken = resetTokenOptional.get();
+            User user = resetToken.getUser();
+    
+            // Update the user's password here
+            user.setPassword(newPassword);  // Ensure to hash the password before saving
+            userRepo.save(user);
+    
+            return "login";
     }
 }
