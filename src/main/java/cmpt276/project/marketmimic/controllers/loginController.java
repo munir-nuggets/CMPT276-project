c0a3f7585b94410d6b7a85ed397512f55eaf17f0
psn4 <psn4@sfu.ca>
2024-07-16 16:41:26 -0700

    removed arbitrary import

--- src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
+++ src/main/java/cmpt276/project/marketmimic/controllers/loginController.java
@@ -2,7 +2,6 @@ package cmpt276.project.marketmimic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
